d88381d9484dcc34797b373061b62b55fe720c80
hoangkha442 <125436413+hoangkha442@users.noreply.github.com>
2024-07-18 12:39:09 +0700

    customer

---
+++ client-customer/src/components/TawkMessengerComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
+
+class TawkMessenger extends Component {
+  render() {
+    return (
+      <TawkMessengerReact propertyId='655c744b91e5c13bb5b225d8' widgetId='1hfoicin0' />
+    );
+  }
+}
+export default TawkMessenger;
