411984b0534f57abaa84d1788e3e52dbda91a59d
hoangkha442 <125436413+hoangkha442@users.noreply.github.com>
2024-07-18 12:37:29 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.ryyohfn.mongodb.net',
+  DB_USER: 'minhman',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'd!@#$u^*Y',
+  JWT_EXPIRES: '31556952000', // in milliseconds (01 year = 31556952000 ms)
+  EMAIL_USER: 'hoangkha020202@gmail.com', // Gmail service
+  EMAIL_PASS: 'olrgaljxrxwqwjrf',
+};
+module.exports = MyConstants;
